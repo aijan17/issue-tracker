4d56232572eb1288473c9c255d11c6ee21f8c089
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-03-11 17:37:45 +0600

    Register models in admin

--- todo/webapp/admin.py
+++ todo/webapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from webapp.models import Task, Type, Status
+
+
+admin.site.register(Task)
+admin.site.register(Type)
+admin.site.register(Status)
