348ca061e19c2552e91f92d7efca5561b90b1266
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-03-14 12:41:02 +0600

    modified types field  ModelMultipleChoiceField to SelectMultiple

--- todo/webapp/forms.py
+++ todo/webapp/forms.py
@@ -1,9 +1,13 @@
 
 from django import forms
+from django.forms import SelectMultiple, Textarea
+
 from webapp.models import Task
 
 
 class TaskForm(forms.ModelForm):
     class Meta:
         model = Task
-        fields = ('summary', 'description', 'status', 'type')
+        fields = ('summary', 'description', 'status', 'types')
+        widgets = {'types':  SelectMultiple(attrs={'class': 'checkbox form-control'}),
+                   'description':Textarea(attrs={'class':'form-control'})}
