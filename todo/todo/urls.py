c86db1fa6e616d5d8c9eed86cc9af9e9af55b26b
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-03-18 18:30:41 +0600

    modified

--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -16,7 +16,7 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from webapp.views import TasksView, AddView, UpdateView, RemoveView, TaskView
+from webapp.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
