d7b8e13c050bd6dcb073fe3619fa12bdc4f7f969
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-03-29 15:16:22 +0600

    added a form for the project model

--- todo/webapp/forms.py
+++ todo/webapp/forms.py
@@ -1,5 +1,6 @@
 from django import forms
-from webapp.models import Task
+
+from webapp.models import Task, Project
 
 
 class TaskForm(forms.ModelForm):
@@ -11,5 +12,16 @@ class TaskForm(forms.ModelForm):
                    }
 
 
+class DateInput(forms.DateInput):
+    input_type = 'date'
 
 
+class ProjectForm(forms.ModelForm):
+    class Meta:
+        model = Project
+        fields = ('title','desc','begin_date','expiration_date','project_task')
+        widgets = {
+            'project_task': forms.SelectMultiple(attrs={'class': 'checkbox form-control'}),
+            'begin_date': DateInput(),
+            'expiration_date': DateInput()
+        }
