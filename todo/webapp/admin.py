918efb660b3c9ea4131e8f45908957b5dffb1d36
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-03-29 15:17:24 +0600

    added the project to the admin register of the model

--- todo/webapp/admin.py
+++ todo/webapp/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from webapp.models import Task, Type, Status
-
+from webapp.models import Task, Type, Status, Project,Projects_tasks
 
 admin.site.register(Task)
+admin.site.register(Project)
 admin.site.register(Type)
 admin.site.register(Status)
+admin.site.register(Projects_tasks)
