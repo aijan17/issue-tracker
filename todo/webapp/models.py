d56fff8d30c282063cd9be3c77f1944010947055
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-04-01 18:25:34 +0600

    updated models

--- todo/webapp/models.py
+++ todo/webapp/models.py
@@ -7,39 +7,30 @@ b = ['@', "$", '!', '#', '%', '^', '*', '~']
 
 
 class Project(models.Model):
-    id = models.AutoField(primary_key=True)
     title = models.CharField(max_length=100, verbose_name='Заголовок')
     desc = models.TextField(max_length=200, verbose_name='Описание')
     begin_date = models.DateField('date')
-    expiration_date = models.DateField(blank=True,null=True)
-    project_task = models.ManyToManyField('Task', verbose_name='Задачи', related_name='projects')
+    expiration_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return '{}: {}'.format(self.pk, self.title, self.desc)
 
 
-class Projects_tasks(models.Model):
-    id = models.AutoField(primary_key=True)
-    tasks_of_project = models.ManyToManyField('Project', blank=True)
-    title = models.CharField(max_length=200, null=False, blank=False)
-    value = models.CharField(max_length=200, null=False, blank=False)
-
-    def __str__(self):
-        return '{}: {}'.format(self.id, self.title, self.value, self.tasks_of_project)
-
-    class Meta:
-        verbose_name = 'Задача проекта'
-        verbose_name_plural = 'Задачи проекта'
-
-
 class Task(models.Model):
     id = models.AutoField(primary_key=True)
     summary = models.CharField(max_length=100, validators=(MinLengthValidator(5),))
-    status = models.ForeignKey('Status', on_delete=models.PROTECT, verbose_name='Статус', null=True, )
+    status = models.ForeignKey('Status', on_delete=models.PROTECT, verbose_name='Статус', null=True)
     types = models.ManyToManyField('Type', blank=True, related_name='types')
     description = models.CharField(max_length=200, validators=(TaskCreateForm(b),))
     create_date = models.DateTimeField(auto_now_add=True)
     update_date = models.DateTimeField(auto_now=True)
+    project = models.ForeignKey('Project',
+                                on_delete=models.CASCADE,
+                                null=True,
+                                verbose_name='Проект',
+                                related_name='tasks',
+                                default=1)
+
 
     def __str__(self):
         return '{}: {}'.format(self.pk, self.summary, self.description)
@@ -67,12 +58,11 @@ class Status(models.Model):
 
 class Type(models.Model):
     id = models.AutoField(primary_key=True)
-    tasks = models.ManyToManyField('Task', blank=True)
     title = models.CharField(max_length=100, null=False, blank=False)
     value = models.CharField(max_length=100, null=False, blank=False)
 
     def __str__(self):
-        return '{}: {}'.format(self.id, self.title, self.value, self.tasks)
+        return '{}: {}'.format(self.id, self.title, self.value)
 
     class Meta:
         verbose_name = 'Тип'
