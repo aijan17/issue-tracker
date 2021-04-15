30b97bdd08bfe07360d4c89a9c7e5192c73043ef
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-04-15 18:23:24 +0600

    added users and group

--- todo/webapp/models.py
+++ todo/webapp/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.urls import reverse
-
+from django.contrib.auth.models import User,Group
 from .Validator import TaskCreateForm, MinLengthValidator
 
 b = ['@', "$", '!', '#', '%', '^', '*', '~']
@@ -9,11 +9,20 @@ b = ['@', "$", '!', '#', '%', '^', '*', '~']
 class Project(models.Model):
     title = models.CharField(max_length=100, verbose_name='Заголовок')
     desc = models.TextField(max_length=200, verbose_name='Описание')
+    users = models.ManyToManyField('auth.User', blank=True, related_name='users', verbose_name='Пользователь')
+    groups = models.ForeignKey('auth.Group', on_delete=models.PROTECT, verbose_name='Группы', null=True)
     begin_date = models.DateField('date')
     expiration_date = models.DateField(blank=True, null=True)
 
     def __str__(self):
         return '{}: {}'.format(self.pk, self.title, self.desc)
+
+    class Meta:
+        verbose_name = 'Проект'
+        verbose_name_plural = 'Проекты'
+        permissions = [
+            ('add_delete_user', 'Добавление и удаление пользователя')
+        ]
 
 
 class Task(models.Model):
@@ -40,6 +49,16 @@ class Task(models.Model):
     class Meta:
         verbose_name = 'Задача'
         verbose_name_plural = 'Задачи'
+
+
+# class Group(models.Model):
+#     id = models.AutoField(primary_key=True)
+#     title = models.CharField(max_length=100, null=False, blank=False)
+#     value = models.CharField(max_length=100, null=False, blank=False)
+#
+#     class Meta:
+#         verbose_name = 'Группа'
+#         verbose_name_plural = 'Группы'
 
 
 class Status(models.Model):
