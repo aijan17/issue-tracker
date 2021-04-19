601d733e0dee4f5706bf0963fa69ae9480b83b2c
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-04-19 17:54:38 +0600

    added path for methods

--- todo/accounts/urls.py
+++ todo/accounts/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 
-from accounts.views import login_view, logout_view, register_view, RegisterView, UserAdd
+from accounts.views import login_view, logout_view, register_view, RegisterView, UserAdd, UserDetailView, ProfilesList
 
 app_name = 'accounts'
 
@@ -9,5 +9,8 @@ urlpatterns = [
     path('logout/', logout_view, name='logout'),
     path('register/', register_view, name='register'),
     path('create/', RegisterView.as_view(), name='create'),
-    path('add/user/<int:pk>/',UserAdd.as_view(),name='add-user')
+    path('add/user/<int:pk>/',UserAdd.as_view(),name='add-user'),
+    path('user/deteil/<int:pk>',UserDetailView.as_view(),name='user-detail'),
+    path('profiles',ProfilesList.as_view(),name='profile')
+
 ]
--- todo/todo/urls.py
+++ todo/todo/urls.py
@@ -13,6 +13,8 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import RedirectView
@@ -24,6 +26,5 @@ urlpatterns = [
     path('admin/',  admin.site.urls),
     path('webapp/', include('webapp.urls')),
     path('accounts/',include('accounts.urls')),
-
     path('', RedirectView.as_view(url='webapp/', permanent=False))
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
