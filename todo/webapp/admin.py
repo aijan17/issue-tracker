2f4af7d4448a243a7c9db8e041f204f2d499b3e3
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-04-19 17:55:04 +0600

    added register for models

--- todo/webapp/admin.py
+++ todo/webapp/admin.py
@@ -1,9 +1,13 @@
 from django.contrib import admin
 
+from accounts.models import Profile
 from webapp.models import Task, Type, Status, Project
 
 admin.site.register(Task)
 admin.site.register(Project)
 admin.site.register(Type)
 admin.site.register(Status)
+admin.site.register(Profile)
+
+
 
