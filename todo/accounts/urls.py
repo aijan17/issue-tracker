4ea9fe608458cc1d41001b2a965ccc39df7a8b61
Aijan Asilbekova <aizhan.asilbekova@gmail.com>
2021-04-22 17:50:31 +0600

    added path to template

--- todo/accounts/urls.py
+++ todo/accounts/urls.py
@@ -1,6 +1,14 @@
 from django.urls import path
 
-from accounts.views import login_view, logout_view, register_view, RegisterView, UserAdd, UserDetailView, ProfilesList
+from accounts.views import login_view, \
+    logout_view, \
+    register_view, \
+    RegisterView, \
+    UserAdd, \
+    UserDetailView, \
+    ProfilesList, \
+    UserChangeView,\
+    UserChangePassword
 
 app_name = 'accounts'
 
@@ -11,6 +19,8 @@ urlpatterns = [
     path('create/', RegisterView.as_view(), name='create'),
     path('add/user/<int:pk>/',UserAdd.as_view(),name='add-user'),
     path('user/deteil/<int:pk>',UserDetailView.as_view(),name='user-detail'),
-    path('profiles',ProfilesList.as_view(),name='profile')
+    path('profiles/',ProfilesList.as_view(),name='profile'),
+    path('profile/update/',UserChangeView.as_view(),name='profile-update'),
+    path('profile/change-password/',UserChangePassword.as_view(),name='change-password')
 
 ]
